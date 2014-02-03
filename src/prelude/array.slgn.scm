8bfe0f8c2cd5c4d66f4d128553c3e8ec4384beb2
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-02-03 09:54:32 +0530

    multi-dimensional arrays. data structure tutorial added.

--- src/prelude/array.slgn.scm
+++ src/prelude/array.slgn.scm
@@ -1,6 +1,14 @@
 ;; Copyright (c) 2013-2014 by Vijay Mathew Pandyalakal, All Rights Reserved.
 
-(define array make-vector)
+(define (array dim #!key fill)
+  (cond ((integer? dim)
+         (make-vector dim fill))
+        ((list? dim)
+         (if (null? (cdr dim))
+             (array (car dim) fill: fill)
+             (array (car dim) fill: (array (cdr dim) fill: fill))))
+        (else (error "invalid array dimension. " dim))))
+
 (define array_length vector-length)
 (define array_at vector-ref)
 (define array_set vector-set!)
