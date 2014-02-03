3de30b9b1e6dbbcca9781caa2d202e24bbdd0797
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-02-03 12:16:43 +0530

    array doc.

--- src/prelude/array.slgn.scm
+++ src/prelude/array.slgn.scm
@@ -13,3 +13,20 @@
 (define array_at vector-ref)
 (define array_set vector-set!)
 (define array_to_list vector->list)
+(define array_copy vector-copy)
+(define subarray subvector)
+(define array_append vector-append)
+(define subarray_fill subvector-fill!)
+(define subarray_move subvector-move!)
+(define array_shrink vector-shrink!)
+
+(define (array_sort arr #!key (test >) (type 'quick))
+  (let ((s (sort (vector->list arr) test: test type: type))
+        (len (vector-length arr)))
+    (let loop ((s s)
+               (i 0))
+      (if (null? s)
+          arr
+          (begin (vector-set! arr i (car s))
+                 (loop (cdr s) (+ i 1)))))))
+    
