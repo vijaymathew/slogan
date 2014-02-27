b784ef39ff2ff08e71c81ac476e067c881f91103
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-02-27 12:25:35 +0530

    tutorial updated.

--- src/prelude/list.slgn.scm
+++ src/prelude/list.slgn.scm
@@ -193,31 +193,31 @@
           r
           (loop (cons fill-with r) (opr n 1))))))
 
-(define (replace lst a b #!key (test *default-eq*) (transform #f) (drill #t))
+(define (replace lst a b #!key (test *default-eq*) (drill #t))
   (let loop ((lst lst)
              (result '()))
     (cond ((null? lst)
            (reverse result))
           ((and (list? (car lst)) drill)
-           (loop (cdr lst) (cons (replace (car lst) a b test: test transform: transform) result)))
+           (loop (cdr lst) (cons (replace (car lst) a b test: test drill: drill) result)))
           ((test a (car lst))
-           (loop (cdr lst) (cons (if transform (transform (car lst) b) b) result)))
+           (loop (cdr lst) (cons b result)))
           (else
            (loop (cdr lst) (cons (car lst) result))))))
 
-(define (replace_all lst alst blst #!key (test eqv?) (transform #f) (drill #t))
+(define (replace_all lst alst blst #!key (test *default-eq*) (transform #f) (drill #t))
   (let loop ((lst lst)
              (result '()))
     (cond ((null? lst)
            (reverse result))
           ((and (list? (car lst)) drill)
-           (loop (cdr lst) (cons (replace_all (car lst) alst blst test: test transform: transform) result)))
+           (loop (cdr lst) (cons (replace_all (car lst) alst blst test: test transform: transform drill: drill) result)))
           (else
            (let inner-loop ((alst alst)
                             (blst blst))
              (cond ((not (null? alst))
                     (if (test (car alst) (car lst))
-                        (loop (cdr lst) (cons (if transform (transform (car lst) (car blst)) (car blst)) result))
+                        (loop (cdr lst) (cons (if transform (transform (car blst)) (car blst)) result))
                         (inner-loop (cdr alst) (cdr blst))))
                    (else 
                     (loop (cdr lst) (cons (car lst) result)))))))))
