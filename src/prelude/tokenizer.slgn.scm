59ff23c99a0fa0ac75f7b575819655cc47015f49
mathew.vijay@gmail.com <mathew.vijay@gmail.com>
2014-03-23 13:46:07 +0530

    bug in read-number fixed.

--- src/prelude/tokenizer.slgn.scm
+++ src/prelude/tokenizer.slgn.scm
@@ -277,8 +277,6 @@
        (or (char-numeric? c)
            (char=? #\e c)
            (char=? #\E c)
-           (char=? c #\+)
-           (char=? c #\-)
            (char=? #\. c))))
 
 (define (char-hex-alpha? c)
