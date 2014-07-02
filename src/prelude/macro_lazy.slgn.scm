29c14417b3e759b1b4a0908d2bbcb1181074d163
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-07-02 17:03:37 +0530

    macro expansion re-written to take care of local variables.

---
+++ src/prelude/macro_lazy.slgn.scm
@@ -0,0 +1,132 @@
+;; Copyright (c) 2013-2014 by Vijay Mathew Pandyalakal, All Rights Reserved.
+
+(define-structure +macro+ params body)
+(define make-macro make-+macro+)
+(define macro-params +macro+-params)
+(define macro-body +macro+-body)
+
+(define *macros* (list (make-table)))
+
+(define (def-macro name macro)
+  (table-set! (car *macros*) name macro))
+
+(define (undef-macro name)
+  (table-set! (car *macros*) name #f))
+
+(define (remove-macro-def name)
+  (if (get-macro-def name)
+      (undef-macro name)))
+
+(define (push-macros)
+  (set! *macros* (cons (make-table) *macros*)))
+
+(define (pop-macros)
+  (set! *macros* (cdr *macros*)))
+
+(define (get-macro-def name)
+  (let loop ((macros *macros*))
+    (if (null? macros)
+        #f
+        (let ((m (table-ref (car macros) name #f)))
+          (if m 
+              m
+              (loop (cdr macros)))))))
+
+(define-structure macro-env bindings)
+
+(define (get-macro-env-value env param default-value)
+  (table-ref (car (macro-env-bindings env)) param default-value))
+
+(define (top-push-macro-env! env)
+  (let ((new-t (make-table))
+        (t (car (macro-env-bindings env))))
+    (table-for-each (lambda (k v) (table-set! new-t k v)) t)
+    (macro-env-bindings-set! env (cons new-t (macro-env-bindings env)))))
+
+(define (update-macro-env! env sym)
+  (let ((t (car (macro-env-bindings env))))
+    (if (not (eq? (table-ref t sym '*unbound*) '*unbound*))
+        (table-set! t sym sym))
+    env))
+
+(define (push-macro-env! env expr extractor)
+  (top-push-macro-env! env)
+  (let ((t (car (macro-env-bindings env))))
+    (let loop ((expr expr))
+      (if (null? expr) 
+          env
+          (let ((sym (extractor expr)))
+            (if (not (eq? (table-ref t sym '*unbound*) '*unbound*))
+                (table-set! t sym sym))
+            (loop (cdr expr)))))))
+
+(define (pop-macro-env! env)
+  (if (null? (macro-env-bindings env))
+      (error "Macro environment bindings cannot be null!")
+      (macro-env-bindings-set! env (cdr (macro-env-bindings env))))
+  env)
+
+(define (params-args->table params args)
+  (let ((t (make-table)))
+    (let loop ((params params)
+               (args args))
+      (if (null? params)
+          t
+          (begin (table-set! t (car params) (car args))
+                 (loop (cdr params) (cdr args)))))))
+
+(define (replace-macro-args params args body)
+  (replace-macro-args-helper 
+   body
+   (make-macro-env (list (params-args->table params args)))))
+
+(define (replace-macro-var params args body)
+  (if (eq? body (car params))
+      (car args)
+      body))
+
+(define (replace-macro-args-helper expr env)
+  (cond ((not (pair? expr))
+         (if (symbol? expr)
+             (get-macro-env-value env expr expr)
+             expr))
+        (else
+         (let ((sym (car expr)))
+           (cond ((or (eq? sym 'let)
+                      (eq? sym 'letrec)
+                      (eq? sym 'let*))
+                  (let ((r (append (list sym (let ((vals (replace-let-vals (cadr expr) env)))
+                                               (if (null? vals)
+                                                   vals
+                                                   (list vals))))
+                                   (replace-macro-args-helper 
+                                    (cddr expr) 
+                                    (push-macro-env! env (cadr expr) caar)))))
+                    (pop-macro-env! env)
+                    r))
+                 ((eq? sym 'lambda)
+                  (let ((r (list sym
+                                 (cadr expr)
+                                 (replace-macro-args-helper
+                                  (cddr expr)
+                                  (push-macro-env! env (cadr expr) car)))))
+                    (pop-macro-env! env)
+                    r))
+                 ((eq? sym 'define)
+                  (let ((r (append (list sym (cadr expr)) 
+                                   (replace-macro-args-helper
+                                    (cddr expr)
+                                    env))))
+                    (update-macro-env! env (cadr expr))
+                    r))
+                 (else (cons (replace-macro-args-helper sym env)
+                             (replace-macro-args-helper (cdr expr) env))))))))
+
+(define (replace-let-vals expr env)
+  (let loop ((expr expr)
+             (result '()))
+    (if (null? expr)
+        result
+        (let ((v (replace-macro-args-helper (cadar expr) env)))
+          (loop (cdr expr) 
+                (append result (cons (caar expr) (list v))))))))
