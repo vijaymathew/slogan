70752dab56d7a8bdc184975d480111e75a7e90c2
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-07-08 17:13:10 +0530

    table interface updated.

--- src/prelude/table.slgn.scm
+++ src/prelude/table.slgn.scm
@@ -1,71 +1,54 @@
 ;; Copyright (c) 2013-2014 by Vijay Mathew Pandyalakal, All Rights Reserved.
 
-(define (table #!key 
-               (size #f)
-               (init #f)
-               (weak_keys #f)
-               (weak_values #f)
-               (test equal?)
-               (hash #f)
-               (min_load 0.45)
-               (max_load 0.90))
-  (cond (size
-         (table-with-size size
-                          init: init
-                          weak-keys: weak_keys
-                          weak-values: weak_values
-                          test: test
-                          hash: hash
-                          min-load: min_load
-                          max-load: max_load))
-        (hash
-         (make-table init: init
-                     weak-keys: weak_keys
-                     weak-values: weak_values
-                     test: test
-                     hash: hash
-                     min-load: min_load
-                     max-load: max_load))
-        (else 
-         (make-table init: init
-                     weak-keys: weak_keys
-                     weak-values: weak_values
-                     test: test
-                     min-load: min_load
-                     max-load: max_load))))
+(define-structure hashtable table eq-predic hash-fn)
 
-(define (table-with-size size #!key 
-                         (init #f)
-                         (weak_keys #f)
-                         (weak_values #f)
-                         (test equal?)
-                         (hash #f)
-                         (min_load 0.45)
-                         (max_load 0.90))
-  (if hash
-      (make-table size: size 
-                  init: init
-                  weak-keys: weak_keys
-                  weak-values: weak_values
-                  test: test
-                  hash: hash
-                  min-load: min_load
-                  max-load: max_load)
-      (make-table size: size 
-                  init: init
-                  weak-keys: weak_keys
-                  weak-values: weak_values
-                  test: test
-                  min-load: min_load
-                  max-load: max_load)))
+(define (make_hashtable hash-fn eq-predic #!optional size)
+  (make-hashtable (if size (make-table hash: hash-fn size: size test: eq-predic)
+                      (make-table hash: hash-fn test: eq-predic))
+                  eq-predic hash-fn))
 
-(define is_table table?)      
-(define table_length table-length)
-(define table_at table-ref)
-(define table_set table-set!)
-(define table_search table-search)
-(define table_for_each table-for-each)
-(define table_to_list table->list)
-(define table_copy table-copy)
-(define table_merge_in_place table-merge!)
-(define table_merge table-merge)
+(define (make_eq_hashtable #!optional size) (make_hashtable eq?-hash eq? size))
+(define (make_eqv_hashtable #!optional size) (make_hashtable eqv?-hash eqv? size))
+(define (make_equal_hashtable #!optional size) (make_hashtable equal?-hash equal? size))
+
+(define symbol_hash symbol-hash)
+(define string_hash string=?-hash)
+(define string_ci_hash string-ci=?-hash)
+(define eq_hash eq?-hash)
+(define eqv_hash eqv?-hash)
+(define equal_hash equal?-hash)
+
+(define (hashtable_equivalence_function ht) (hashtable-eq-predic ht))
+(define (hashtable_hash_function ht) (hashtable-hash-fn ht))
+
+(define (hashtable_set ht key obj) (table-set! (hashtable-table ht) key obj))
+
+(define (hashtable_at ht key #!optional default) (table-ref (hashtable-table ht) key default))
+
+(define (hashtable_contains ht key)
+  (let ((table (hashtable-table ht))
+        (predic (hashtable-eq-predic ht)))
+    (table-search (lambda (k v) (predic k key)) table)))
+
+(define (hashtable_update ht key proc #!optional default)
+  (let ((table (hashtable-table ht)))
+    (table-set! table key (proc (table-ref table key default)))))
+
+(define (hashtable_size ht) (table-length (hashtable-table ht)))
+
+(define (hashtable_copy ht) (make-hashtable (table-copy (hashtable-table ht))
+                                            (hashtable-eq-predic ht) 
+                                            (hashtable-hash-fn ht)))
+
+(define (hashtable_keys ht)
+  (let ((keys '()))
+    (table-for-each (lambda (k v) (set! keys (cons k keys))) (hashtable-table ht))
+    (reverse keys)))
+
+(define (hashtable_entries ht)
+  (let ((e '()))
+    (table-for-each (lambda (k v) (set! e (cons v e))) (hashtable-table ht))
+    (reverse e)))
+
+(define (hashtable_for_each proc ht)
+  (table-for-each proc (hashtable-table ht)))
