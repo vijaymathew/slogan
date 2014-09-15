6f1516b67cfc08709f6d51c465e36af0e5c02023
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2014-09-15 16:54:09 +0530

    filesystem functions refactored.

--- src/prelude/os.slgn.scm
+++ src/prelude/os.slgn.scm
@@ -4,8 +4,10 @@
 ;; These functions are specific to this implementation of Slogan.
 ;; Right now we have some of the most useful Gambit "Host environment" functions here.
 
-(define (list_directory dirname #!key ignore_hidden)
-  (let ((d (open-directory (list path: dirname ignore-hidden: ignore_hidden))))
+(define (list_directory dirname #!optional (ignore_hidden #t))
+  (let ((d (open-directory (list path: dirname ignore-hidden: (if (symbol? ignore_hidden) 
+                                                                  (slgn-symbol->scm-sym/kw ignore_hidden string->symbol) 
+                                                                  ignore_hidden)))))
     (let ((ls (read-all d)))
       (close-input-port d)
       ls)))
@@ -39,7 +41,32 @@
 (define file_exists file-exists?)
 (define file_info file-info)
 (define is_file_info file-info?)
-(define file_info_type file-info-type)
+
+(define (file_info_type fi)
+  (scm-symbol->slgn-sym (file-info-type fi)))
+
+(define file_info_device file-info-device)
+(define file_info_inode file-info-inode)
+(define file_info_mode file-info-mode)
+(define file_info_number_of_links file-info-number-of-links)
+(define file_info_owner file-info-owner)
+(define file_info_group file-info-group)
+(define file_info_size file-info-size)
+
+(define (file_info_last_access_time fi)
+  (time->seconds (file-info-last-access-time fi)))
+
+(define (file_info_last_modification_time fi)
+  (time->seconds (file-info-last-modification-time fi)))
+
+(define (file_info_last_change_time fi)
+  (time->seconds (file-info-last-change-time fi)))
+
+(define file_info_attributes file-info-attributes)
+
+(define (file_info_creation_time fi)
+  (time->seconds (file-info-creation-time fi)))
+
 (define file_type file-type)
 (define file_device file-device)
 (define file_inode file-inode)
@@ -48,12 +75,26 @@
 (define file_owner file-owner)
 (define file_group file-group)
 (define file_size file-size)
-(define file_last_access_time file-last-access-time)
-(define file_last_modification_time file-last-modification-time)
-(define file_last_change_time file-last-change-time)
+
+(define (file_last_access_time path)
+  (time->seconds (file-last-access-time path)))
+
+(define (file_last_modification_time path)
+  (time->seconds (file-last-modification-time path)))
+
+(define (file_last_change_time path)
+  (time->seconds (file-last-change-time path)))
+
 (define file_attributes file-attributes)
-(define file_creation_time file-creation-time)
 
+(define (file_creation_time path)
+  (time->seconds (file-creation-time path)))
 
+(define host_name host-name)
+(define host_info host-info)
+(define is_host_info host-info?)
+(define host_info_name host-info-name)
+(define host_info_aliases host-info-aliases)
+(define host_info_addresses host-info-addresses)
 
   
