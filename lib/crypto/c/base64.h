69a353a50e2c51d05a320367894f1a86f6d200de
vijaymathew <mathew.vijay@gmail.com>
2015-12-31 14:17:01 +0530

    removed lib folder - to be replaced by packages

--- lib/crypto/c/base64.h
+++
@@ -1,7 +0,0 @@
-#ifndef BASE64_H
-#define BASE64_H
-
-void base64_encode(const unsigned char *input, int len, unsigned char *output);
-int base64_decode(const unsigned char *input, int len, unsigned char *output);
-
-#endif
