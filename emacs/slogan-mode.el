131e03c6f53d78d18591fa47cb1acacd211fbbff
vijaymathew <mathew.vijay@gmail.com>
2016-03-17 09:01:51 +0530

    #18 - auto-comment support and sytax table

--- emacs/slogan-mode.el
+++ emacs/slogan-mode.el
@@ -13,10 +13,24 @@
       `((,slogan-constant-regexp . font-lock-constant-face)
         (,slogan-keywords-regexp . font-lock-keyword-face)))
 
-(define-derived-mode slogan-mode fundamental-mode
-  "slogan mode"
+(defvar slogan-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" st)
+    (modify-syntax-entry ?/ ". 124b" st)
+    (modify-syntax-entry ?* ". 23" st)
+    (modify-syntax-entry ?\n "> b" st)
+    st)
+  "Syntax table for slogan-mode")
+    
+(define-derived-mode slogan-mode fundamental-mode "slogan mode"
   "Major mode for editing Slogan programs"
-  (setq font-lock-defaults '((slogan-font-lock-keywords))))
+  :syntax-table slogan-mode-syntax-table
+  (setq font-lock-defaults '((slogan-font-lock-keywords)))
+  (setq-local comment-start "/*")
+  (setq-local comment-start-skip "/\\*+[ \t]*")
+  (setq-local comment-end "*/")
+  (setq-local comment-end-skip "[ \t]*\\*+/")
+  (setq-local parse-sexp-ignore-comments t))
 
 (setq slogan-keywords nil)
 (setq slogan-constants nil)
