2bb100a523f4247fa984f0c15af8dca1e16bbe5a
vijaymathew <mathew.vijay@gmail.com>
2017-03-05 20:30:48 +0530

    #115 - core names in a module, first commit

--- src/core/consts.slgn.scm
+++ src/core/consts.slgn.scm
@@ -1,7 +1,7 @@
 ;; Copyright (C) 2013-2017 by Vijay Mathew Pandyalakal <vijay.the.lisper@gmail.com>
 
 (define *major-version* 0)
-(define *minor-version* 11.93)
+(define *minor-version* 12.0)
 (define *release-name* 'beta)
 
 (define *scm-extn* ".scm")
